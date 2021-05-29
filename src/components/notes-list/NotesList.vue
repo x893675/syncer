f724cc250c412fdf326be03cd1b1c3e76c520255
x893675 <x893675@icloud.com>
2021-05-29 14:24:47 +0800

    init electron project

---
+++ src/components/notes-list/NotesList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="notes-list">
+    <div id="search-buttons" class="columns">
+      <div id="search">
+        <b-field>
+          <b-input
+            placeholder="Search by time..."
+            v-model="searchField"
+            type="search"
+            icon="search"
+          ></b-input>
+        </b-field>
+      </div>
+      <div id="buttons">
+        <button
+          id="add-note"
+          class="button is-dark"
+          @click="showCreateNoteModal()"
+        >
+          <b-icon icon="plus"></b-icon>
+        </button>
+      </div>
+    </div>
+    <b-modal :active.sync="aboutModalActive" has-modal-card>
+      <cn-about-modal></cn-about-modal>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import AboutModal from "../modals/about-modal/AboutModal";
+
+export default {
+  name: "cn-notes-list",
+  components: {
+    "cn-about-modal": AboutModal
+  },
+  data() {
+    return {
+      searchField: "",
+      aboutModalActive: false
+    };
+  }
+};
+</script>
+
+<style src="./NotesList.scss" lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import axios from "axios";
+import Buefy from "buefy";
+import VueClipboard from "vue-clipboard2";
+import VModal from "vue-js-modal";
+import "font-awesome/css/font-awesome.min.css";
+import "source-sans-pro/source-sans-pro.css";
+
+import App from "./App";
+
+import router from "./router";
+import store from "./store";
+// import db from "./datastore-notes";
+
+//const electron = require("electron");
+// electron.ipcRenderer.on("about", (arg, event) => {
+//   let component = router.currentRoute.matched[0].instances.default;
+//   if (event === "about-modal-active") {
+//     component.aboutModalActive = true;
+//   }
+// });
+
+Vue.use(Buefy, {
+  defaultIconPack: "fa"
+});
+
+Vue.use(VueClipboard);
+
+Vue.use(VModal);
+
+// Vue.filter("capitalize", value => {
+//   if (!value) return "";
+//   value = value.toString();
+//   return value.charAt(0).toUpperCase() + value.slice(1);
+// });
+
+// if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+// Vue.prototype.$db = db;
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: "<App/>"
+}).$mount("#app");
